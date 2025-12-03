a41b73cbecb4d24905dc5127512ad3f7bbff7e76
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-03 16:08:55 +0530

    set up and connect MongoDB

---
+++ Youtube/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose  from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+    try{
+        let responce = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Data base conneted : ",responce.connection.host);   
+    }
+    catch (e){ 
+        console.error("Error in DB Connection" , e);
+    }
+}
+
+export default connectDB;
