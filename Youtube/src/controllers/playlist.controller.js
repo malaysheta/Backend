162f4118ca1c5eb94edbbef7594d18f111f6ff6c
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-09 15:26:28 +0530

    Add video management functionalities to playlists

--- Youtube/src/controllers/playlist.controller.js
+++ Youtube/src/controllers/playlist.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 import  ApiError  from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { Playlist } from '../models/playlist.model.js'
+import { Video } from '../models/video.model.js'
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description=""} = req.body
@@ -62,6 +63,142 @@ const getPlaylistById = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200,playlists,"playlist fetched successfully"))
 })
 
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.query
+     
+    if(!playlistId || !videoId){
+        throw new ApiError(401,"playlist id  and video id are required")
+    }
+    const user = req.user
+    if (!user) {
+        throw new ApiError(403,"unathorized access login first")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist){
+        throw new ApiError(401,"Playlist not found")
+    }
+
+    if(playlist.owner.toString() != user._id.toString()){
+        throw new ApiError(403, "Anathorized access , you can not edits others playlist")
+    }
+
+    const video =await Video.findById(videoId);
+    
+    if(!video){
+        throw new ApiError(401,"Video not found")
+    }
+
+    if (playlist.videos.includes(videoId)) {
+        throw new ApiError(409, "Video already exists in playlist");
+    }
+
+    playlist.videos.push(videoId);
+    await playlist.save();
+
+    res.status(200).json(
+        new ApiResponse(200, playlist, "Video added to playlist successfully")
+    );
+})
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.query
+    // TODO: remove video from playlist
+    if(!playlistId || !videoId){
+        throw new ApiError(401,"playlist id  and video id are required")
+    }
+    const user = req.user
+    if (!user) {
+        throw new ApiError(403,"unathorized access login first")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist){
+        throw new ApiError(401,"Playlist not found")
+    }
+
+    if(playlist.owner.toString() != user._id.toString()){
+        throw new ApiError(403, "Anathorized access , you can not edits others playlist")
+    }
+
+    const video =await Video.findById(videoId);
+    
+    if(!video){
+        throw new ApiError(401,"Video not found")
+    }
+    if (!playlist.videos.includes(videoId)) {
+        throw new ApiError(409, "Video not exists in playlist");
+    }
+    
+    playlist.videos.remove(videoId);
+    await playlist.save();
+
+    res.status(200).json(
+        new ApiResponse(200, playlist, "Video deleted to playlist successfully")
+    );
+
+})
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    // TODO: delete playlist
+    if(!playlistId){
+        throw new ApiError(400,"playlist id is required")
+    }
+    const user = req.user
+    if (!user) {
+        throw new ApiError(401,"unathorized access login first")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist){
+        throw new ApiError(404,"Playlist not found")
+    }
+
+    if(playlist.owner.toString() != user._id.toString()){
+        throw new ApiError(403, "Anathorized access , you can not delete others playlist")
+    }
+    
+    await playlist.deleteOne();
+
+    res.status(200).json(new ApiResponse(200,"Playlist deleted successfully"))
+})
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+
+    if(!playlistId){
+        throw new  ApiError(400,"Playlist id is required")
+    }
+    if(!name && !description){
+        throw new ApiError(400,"name or description is required")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlist){
+        throw new ApiError(400,"playlist not found")
+    }
+    const user = req.user;
 
 
-export { createPlaylist , getUserPlaylists , getPlaylistById}
+    if(playlist.owner.toString() !== user._id.toString()){
+        throw new ApiError(403,"You are not Aothorized user , tou can not edit otheres playlist")
+    }
+
+    if(name) playlist.name = name
+    if(description) playlist.description = description
+
+    const updatedPlaylist = await playlist.save();
+    if(!updatedPlaylist){
+        throw new ApiError(500, "internal server error , Enable to update the details")
+    }
+
+    res.status(200).json(new ApiResponse(200,updatedPlaylist,"details updated sunccessfull"));
+    //TODO: update playlist
+})
+
+export { createPlaylist , getUserPlaylists , getPlaylistById , addVideoToPlaylist , removeVideoFromPlaylist , deletePlaylist , updatePlaylist}
--- Youtube/src/routes/playlist.route.js
+++ Youtube/src/routes/playlist.route.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from '../middlewares/auth.middleware.js'
-import { createPlaylist , getUserPlaylists , getPlaylistById} from "../controllers/playlist.controller.js";
+import { createPlaylist , getUserPlaylists , getPlaylistById , addVideoToPlaylist , removeVideoFromPlaylist , deletePlaylist , updatePlaylist} from "../controllers/playlist.controller.js";
 
 const router = Router()
 
 router.route('/create-playlist').post(verifyJWT , createPlaylist)
 router.route('/get-playlist').get(verifyJWT , getUserPlaylists)
 router.route('/get-playlistbyid/:playlistId').get(getPlaylistById)
-
+router.route('/add-video').post(verifyJWT,addVideoToPlaylist)
+router.route('/remove-video').post(verifyJWT,removeVideoFromPlaylist)
+router.route('/delete-playlist/:playlistId').delete(verifyJWT,deletePlaylist)
+router.route('/update-playlist/:playlistId').patch(verifyJWT,updatePlaylist)
 
 export default router;
