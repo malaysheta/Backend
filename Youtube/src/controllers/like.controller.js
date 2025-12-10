17adfad2ff75dceeebce0825746111aa9d44aace
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-10 14:13:36 +0530

    Add toggleTweetLike and getLikedvideo functionality

--- Youtube/src/controllers/like.controller.js
+++ Youtube/src/controllers/like.controller.js
@@ -59,7 +59,49 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
     
         res.status(200).json(new ApiResponse(200,like,"comments has been liked succesfully"))
     }
-            
+           
 })
 
-export { toggleVideoLike , toggleCommentLike}
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    //TODO: toggle like on tweet
+
+     const user = req.user;
+    
+    if(!tweetId){
+        throw new ApiError(400,"tweet id is required")
+    }
+    if(!user){
+        throw new ApiError(401,"User should be logged in")
+    }
+
+    const isAlreadyLiked = await Like.findOne({tweet : tweetId , likedBy : user._id});
+
+    if(isAlreadyLiked){
+        await Like.findOneAndDelete({tweet : tweetId , likedBy : user._id});
+        res.status(200).json(new ApiResponse(200,"tweet like has been removed successfully"));
+    }
+    else{
+        const like = await Like.create({
+            tweet : tweetId,
+            likedBy : user._id,
+        })
+        res.status(200).json(new ApiResponse(200,like,"tweet has been liked succesfully"))
+    }
+})
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+    const user = req.user
+    if(!user){
+        throw new ApiError(401,"Login required")
+    }
+    const likedVideos = await Like.find({
+        likedBy: req.user._id,
+        video: { $ne: null }   // only video likes
+    }).populate("video");
+
+    res.status(200).json(new ApiResponse(200,likedVideos,"All liked video fetched successfully"))
+})
+
+export { toggleVideoLike , toggleCommentLike , toggleTweetLike , getLikedVideos}
--- Youtube/src/routes/like.route.js
+++ Youtube/src/routes/like.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT} from '../middlewares/auth.middleware.js'
-import { toggleCommentLike, toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleCommentLike, toggleVideoLike , toggleTweetLike , getLikedVideos} from "../controllers/like.controller.js";
 const router = Router()
 
 router.route('/toggel-video-like/:videoId').post(verifyJWT , toggleVideoLike )
 router.route('/toggel-comment-like/:commentId').post(verifyJWT, toggleCommentLike)
+router.route('/toggel-tweet-like/:tweetId').post(verifyJWT, toggleTweetLike)
+router.route('/get-liked-video').get(verifyJWT,getLikedVideos)
 
 export default router;
