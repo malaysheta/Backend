15e499ba5cb1b7c59ed5450817ff73bec0fb4c7c
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-10 15:13:29 +0530

    Add getChannelStats controller and route for channel statistics

--- Youtube/src/controllers/dashboard.controller.js
+++ Youtube/src/controllers/dashboard.controller.js
@@ -6,10 +6,6 @@ import ApiError from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-
-})
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
@@ -20,6 +16,18 @@ const getChannelVideos = asyncHandler(async (req, res) => {
     const allvideo = await Video.find({owner : user._id});
     res.status(200).json(new ApiResponse(200,allvideo,"All videos of channel is fetched successfully"))
 })
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const user = req.user;
+    //total subscriber ;
+    const totalsub = (await Subscription.find({ channel : user._id })).length;
+    const videos = await Video.find({owner : user._id});
+
+    return res.status(200).json(new ApiResponse(200,{totalsub , videos},"Dashboard details feched successfully"));
+})
+
+
 
 export {
     getChannelStats, 
--- Youtube/src/routes/dashboard.route.js
+++ Youtube/src/routes/dashboard.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getChannelVideos } from "../controllers/dashboard.controller.js";
+import { getChannelVideos , getChannelStats} from "../controllers/dashboard.controller.js";
 
 const router = Router();
 
 router.route('/myvideo').get(verifyJWT , getChannelVideos)
+router.route('/channel-stats').get(verifyJWT,getChannelStats);
 export default router;
